e130f20969d9fc920e7ee8497d66c6c09df9eb92
Ritesh Bobade <cdriteshbobade@gmail.com>
2026-02-20 20:44:14 +0530

    latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Verify from './pages/verify'
