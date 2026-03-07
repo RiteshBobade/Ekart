4308919b6e34aa333a0dfec77be369e38d079f52
Ritesh Bobade <cdriteshbobade@gmail.com>
2026-03-07 11:45:56 +0530

    Ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,19 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 
@@ -22,8 +33,14 @@ app.use("/api/v1/product", productRoute);
 app.use("/api/v1/cart", cartRoute);
 app.use("/api/v1/orders", orderRoute);
 
+app.get("/", (req, res) => {
+  res.send("Ekart API is running!");
+});
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 });
+
+export default app;
